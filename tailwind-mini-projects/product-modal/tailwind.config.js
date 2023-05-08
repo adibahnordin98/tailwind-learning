d686b1e53b06ade1334d923ad210316003eae631
adibahnordin98 <adibah.nordin23@gmail.com>
2023-05-08 12:32:29 +0800

    add card modal mini project

---
+++ tailwind-mini-projects/product-modal/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./dist/*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
